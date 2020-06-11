c60817998717ebd41158f9d79ce0a13ff4497032
bolja95 <bolja95@gmail.com>
2020-06-11 17:09:34 +0200

    pakovanje

--- ProjekatRES/Biblioteka/CollectionDescription.cs
+++ ProjekatRES/Biblioteka/CollectionDescription.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DumpingBuffer
+namespace Biblioteka
 {
     public class CollectionDescription
     {
         public int id { get; set; }
         public int dataset { get; set; }
         public List<DumpingProperty> DumpingPropertyCollection { get; set; }
+        public bool poslat { get; set; }
 
         public CollectionDescription(int a, int b, List<DumpingProperty> c)
         {
             id = a;
             dataset = b;
             DumpingPropertyCollection = c;
+            poslat = false;
         }
     }
 }
